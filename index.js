d02930f7f735930fc49381aaf07b10f20a2126c9
Stu Wares <stuwares@outlook.com>
2022-09-23 12:54:35 +0100

    switched to v2 api

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import { rwClient } from "./twitterClient.js";
 
 const tweet = async () => {
     try {
-        await rwClient.v1.tweet('beep boop');
+        await rwClient.v2.tweet('beep boop');
 
     } catch (e) {
         console.error(e);
